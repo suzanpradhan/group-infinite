137404f126894b4dec3365fb1fc9250e73ea6dde
suzanpradhan <sujanpradhan478@gmail.com>
2021-06-26 01:31:28 +0545

    Laravel Full Setup to Design

---
+++ group-infinite/resources/views/inc/header-metas.blade.php
@@ -0,0 +1,6 @@
+<!-- CSS -->
+<link rel="stylesheet" href="{{asset('/css/app.css')}}" />
+
+<!-- SCRIPTS -->
+<script src="https://use.fontawesome.com/1c36f3a8f0.js"></script>
+<script src="https://unpkg.com/eva-icons"></script>
